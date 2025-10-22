a37042eeb1a2f9ea6ac9146baa5452cca3925a0d
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-10-22 09:19:08 -0300

    feat: create BuildingSection

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'; 
 import Main from './components/Main.vue'; 
+import BuildingSection from './components/BuildingSection.vue';
 </script>
 
 <template>
   <div class="px-4 sm:px-6 lg:px-52">
     <Navbar />
-
     <Main />
+    <BuildingSection/>
   </div>
 </template>
---
+++ src/components/BuildingSection.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="grid grid-cols-4 gap-6 max-w-8xl mx-auto m-7">
+        <div class="max-w-sm  bg-white border border-gray-200 rounded-[30px] shadow-lg">
+            <a href="#">
+                <img 
+                    style="border-top-left-radius: 20px; border-top-right-radius: 20px;"
+                    src="/src/assets/images/predio1.jpg" 
+                    alt="Predio empresarial"
+                />
+            </a>
+            <div class="p-3">
+                <a href="#">
+                    <h5 class="text-center mt-3 text-base font-semibold tracking-tight text-gray-800">
+                        Duplex & Triplex
+                    </h5>
+                </a>
+            </div>
+        </div>
+
+        <div class="max-w-sm  bg-white border border-gray-200 rounded-[30px] shadow-lg">
+            <a href="#">
+                <img 
+                    style="border-top-left-radius: 20px; border-top-right-radius: 20px;"
+                    src="/src/assets/images/predio5.jpg" 
+                    alt="Predio empresarial"
+                />
+            </a>
+            <div class="p-3">
+                <a href="#">
+                    <h5 class="text-center mt-3 text-base font-semibold tracking-tight text-gray-800">
+                        Apartamento e Unidades
+                    </h5>
+                </a>
+            </div>
+        </div>
+
+        <div class="max-w-sm  bg-white border-2 border-[#1F3777] rounded-[30px] shadow-lg overflow-hidden">
+            <a href="#">
+                <img 
+                    src="/src/assets/images/predio2.jpg" 
+                    alt="Predio empresarial"
+                />
+            </a>
+            <div class="p-3">
+                <a href="#">
+                    <h5 class="text-center mb-2 mt-1 text-base font-semibold tracking-tight text-gray-800">
+                        Edifícios de vários andares
+                    </h5>
+                </a>
+            </div>
+        </div>
+
+        <div class="max-w-sm  bg-white border border-gray-200 rounded-[30px] shadow-lg">
+            <a href="#">
+                <img 
+                    style="border-top-left-radius: 20px; border-top-right-radius: 20px;"
+                    src="/src/assets/images/predio4.jpg" 
+                    alt="Predio empresarial"
+                />
+            </a>
+            <div class="p-3">
+                <a href="#">
+                    <h5 class="text-center mb-2 mt-1 text-base font-semibold tracking-tight text-gray-800">
+                        Arquitetura Moderna
+                    </h5>
+                </a>
+            </div>
+        </div>
+    </section>
+</template>

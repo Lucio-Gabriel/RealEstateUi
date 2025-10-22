ed20d0b9965115fb809b73340d5597843a98a8ae
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-10-22 08:00:36 -0300

    feat: width icons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,7 +7,7 @@
             <div class="flex items-center">
                 <div class="relative grow">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building-community w-4 h-4 text-gray-500 dark:text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-building-community w-5 h-5 text-gray-500 dark:text-gray-400">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8" />
                             <path d="M13 7l0 .01" /><path d="M17 7l0 .01" /><path d="M17 11l0 .01" />
                             <path d="M17 15l0 .01" />
@@ -15,7 +15,7 @@
                     </div>
                     <input type="search" class="w-full ps-10 pe-10 py-2 bg-slate-200 rounded-full border-none focus:ring-0 focus:outline-none" placeholder="Encontre Seu Imóvel">
                     <div class="absolute inset-y-0 end-0 flex items-center pe-3 pointer-events-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-horizontal w-4 h-4 text-gray-500 dark:text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-horizontal w-5 h-5 text-gray-500 dark:text-gray-400">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M4 6l8 0" /><path d="M16 6l4 0" />
                             <path d="M8 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M4 12l2 0" /><path d="M10 12l10 0" /><path d="M17 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                             <path d="M4 18l11 0" /><path d="M19 18l1 0" />

8504a5cf6bbfc43d381c8e4e646d881fe3b8f6c6
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-10-23 20:41:17 -0300

    feat: create components svg

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import Clock from './svg/Clock.vue';
+import HouseCash from './svg/HouseCash.vue';
+import HouseSec from './svg/HouseSec.vue';
+import Medal from './svg/Medal.vue';
+
+</script>
+
 <template>
     <section class="max-w-6xl mx-auto m-32">
         <div class="flex flex-col items-center justify-center">
@@ -14,14 +22,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 mt-10 justify-center">
             <div class="bg-primary flex gap-5 py-10 px-5  rounded-[16px] md:ml-5 mt-3">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-home-dollar text-white w-16 mt-3">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M19 10l-7 -7l-9 9h2v7a2 2 0 0 0 2 2h6" />
-                        <path d="M9 21v-6a2 2 0 0 1 2 -2h2c.387 0 .748 .11 1.054 .3" />
-                        <path d="M21 15h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" />
-                        <path d="M19 21v1m0 -8v1" />
-                    </svg>
+                    <HouseCash />
                 </div>
 
                 <div class="flex flex-col gap-3">
@@ -35,12 +36,7 @@
 
             <div class="bg-slate-300 flex gap-5 py-10 px-5  rounded-[16px] md:ml-5 mt-3">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 4v3m-4 -3v6m8 -6v6" />
-                        <path d="M12 18.5l-3 1.5l.5 -3.5l-2 -2l3 -.5l1.5 -3l1.5 3l3 .5l-2 2l.5 3.5z" />
-                    </svg>
+                    <Medal />
                 </div>
 
                 <div class="flex flex-col gap-3">
@@ -54,12 +50,7 @@
 
             <div class="bg-slate-300 flex gap-5 py-10 px-5  rounded-[16px] md:ml-5 mt-3">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 4v3m-4 -3v6m8 -6v6" />
-                        <path d="M12 18.5l-3 1.5l.5 -3.5l-2 -2l3 -.5l1.5 -3l1.5 3l3 .5l-2 2l.5 3.5z" />
-                    </svg>
+                    <Clock />
                 </div>
 
                 <div class="flex flex-col gap-3">
@@ -73,12 +64,7 @@
 
             <div class="bg-slate-300 flex gap-5 py-10 px-5  rounded-[16px] md:ml-5 mt-3">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
-                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 13m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                        <path d="M12 10l0 3l2 0" /><path d="M7 4l-2.75 2" /><path d="M17 4l2.75 2" />
-                    </svg>
+                    <HouseSec />
                 </div>
 
                 <div class="flex flex-col gap-3">
---
+++ src/components/svg/Clock.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
+        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 13m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+        <path d="M12 10l0 3l2 0" /><path d="M7 4l-2.75 2" /><path d="M17 4l2.75 2" />
+    </svg>
+</template>

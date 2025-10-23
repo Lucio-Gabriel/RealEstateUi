eb887ae15a225e632bf3823174b22dc24a45326e
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-10-23 09:03:33 -0300

    feat: WIP services

---
+++ src/components/Services.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="max-w-6xl mx-auto m-32">
+        <div class="flex flex-col items-center justify-center">
+            <h2 class="font-semibold text-5xl">
+                Nossos Serviços
+            </h2>
+
+            <p class="max-w-md text-center text-slate-600 font-normal mt-10">
+                Melhore seus anúncios de imóveis e vídeos com
+                legendas precisas e envolventes.
+            </p>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 mt-10 justify-center">
+            <div class="bg-primary flex gap-5 py-10 px-5 max-w-md rounded-[30px] md:ml-32">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-home-dollar text-white w-16 mt-3">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M19 10l-7 -7l-9 9h2v7a2 2 0 0 0 2 2h6" />
+                        <path d="M9 21v-6a2 2 0 0 1 2 -2h2c.387 0 .748 .11 1.054 .3" />
+                        <path d="M21 15h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" />
+                        <path d="M19 21v1m0 -8v1" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col gap-3">
+                    <h3 class="text-xl text-white font-semibold">Impostos acessíveis</h3>
+
+                    <p class="max-w-xs text-slate-400 text-base">
+                        Ajudamos você a encontrar um novo lar, oferecendo uma experiência imobiliária
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-slate-300 flex gap-5 py-10 px-5 max-w-md rounded-[30px] md:ml-5">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 4v3m-4 -3v6m8 -6v6" />
+                        <path d="M12 18.5l-3 1.5l.5 -3.5l-2 -2l3 -.5l1.5 -3l1.5 3l3 .5l-2 2l.5 3.5z" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col gap-3">
+                    <h3 class="text-xl text-black font-semibold">Casas de qualidade garantida</h3>
+
+                    <p class="max-w-xs text-black text-base">
+                        Ajudamos você a encontrar um novo lar, oferecendo uma experiência imobiliária
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-slate-300 flex gap-5 py-10 px-5 max-w-md rounded-[30px] md:ml-32 mt-3">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M12 13m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                        <path d="M12 10l0 3l2 0" /><path d="M7 4l-2.75 2" /><path d="M17 4l2.75 2" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col gap-3">
+                    <h3 class="text-xl text-black font-semibold">Processo rápido e fácil</h3>
+
+                    <p class="max-w-xs text-black text-base">
+                        Ajudamos você a encontrar um novo lar, oferecendo uma experiência imobiliária
+                    </p>
+                </div>
+            </div>
+
+            <div class="bg-slate-300 flex gap-5 py-10 px-5 max-w-md rounded-[30px] md:ml-5 mt-3">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" 
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-medal text-primary w-16 mt-3">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9 21v-6a2 2 0 0 1 2 -2h1.341" />
+                        <path d="M19.682 10.682l-7.682 -7.682l-9 9h2v7a2 2 0 0 0 2 2h5" />
+                        <path d="M22 16c0 4 -2.5 6 -3.5 6s-3.5 -2 -3.5 -6c1 0 2.5 -.5 3.5 -1.5c1 1 2.5 1.5 3.5 1.5z" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col gap-3">
+                    <h3 class="text-xl text-black font-semibold">Seguro de propriedade</h3>
+
+                    <p class="max-w-xs text-black text-base">
+                        Ajudamos você a encontrar um novo lar, oferecendo uma experiência imobiliária
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
